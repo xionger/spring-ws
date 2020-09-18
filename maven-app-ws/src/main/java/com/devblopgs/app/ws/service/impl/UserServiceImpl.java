67da2a1881ccc9dc14cf77ad87e14b97ffa1912a
Xunhao Xiong <xxiong42@gatech.edu>
2020-09-18 16:17:34 -0500

    autowire UserRepository

--- maven-app-ws/src/main/java/com/devblopgs/app/ws/service/impl/UserServiceImpl.java
+++ maven-app-ws/src/main/java/com/devblopgs/app/ws/service/impl/UserServiceImpl.java
@@ -1,14 +1,33 @@
 package com.devblopgs.app.ws.service.impl;
 
+import com.devblopgs.app.ws.io.entity.UserEntity;
+import com.devblopgs.app.ws.io.repository.UserRepository;
 import com.devblopgs.app.ws.service.UserService;
 import com.devblopgs.app.ws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, userDto);
+
+        return userDto;
     }
 }
+
