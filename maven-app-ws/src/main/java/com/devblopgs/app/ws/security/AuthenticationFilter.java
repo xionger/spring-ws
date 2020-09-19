4b526e7013fb0389a7fee534562b06c40d5422dc
Xunhao Xiong <xxiong42@gatech.edu>
2020-09-19 12:49:29 -0500

    implement authentication filter

---
+++ maven-app-ws/src/main/java/com/devblopgs/app/ws/security/AuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.devblopgs.app.ws.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.devblopgs.app.ws.service.UserService;
+import com.devblopgs.app.ws.shared.dto.UserDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.devblopgs.app.ws.ui.model.request.UserLoginRequestModel;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager manager) {
+        this.authenticationManager = manager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+            UserLoginRequestModel creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), UserLoginRequestModel.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String userName = ((User) auth.getPrincipal()).getUsername();
+
+        // Jwts docs: https://github.com/jwtk/jjwt
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET )
+                .compact();
+        // UserService userService = (UserService)SpringApplicationContext.getBean("userServiceImpl");
+        // UserDto userDto = userService.getUser(userName);
+
+        res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+        // res.addHeader("UserID", userDto.getUserId());
+
+    }
+}
--- maven-app-ws/src/main/java/com/devblopgs/app/ws/security/SecurityConstants.java
+++ maven-app-ws/src/main/java/com/devblopgs/app/ws/security/SecurityConstants.java
@@ -4,6 +4,7 @@ public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final long PASSWORD_RESET_EXPIRATION_TIME = 3600000; // 1 hour
     public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_SECRET = "a7b5c4d2e5fg9";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
     public static final String VERIFICATION_EMAIL_URL = "/users/email-verification";
