4035539050616a807b5f9be335090d8c1861e7f0
Xunhao Xiong <xxiong42@gatech.edu>
2020-09-19 23:07:51 -0500

    customize error object

--- maven-app-ws/src/main/java/com/devblopgs/app/ws/exception/AppExceptionsHandler.java
+++ maven-app-ws/src/main/java/com/devblopgs/app/ws/exception/AppExceptionsHandler.java
@@ -2,6 +2,7 @@ package com.devblopgs.app.ws.exception;
 
 import java.util.Date;
 
+import com.devblopgs.app.ws.ui.model.response.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +17,17 @@ public class AppExceptionsHandler {
     @ExceptionHandler(value = {UserServiceException.class})
     public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request)
     {
-        //ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 
-        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<Object> handleOtherExceptions(Exception ex, WebRequest request)
     {
-        //ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 
-        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
